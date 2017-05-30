9e4c9beb636a932926a80a9b81dcaaeed69ade70
Vasil <vsl.h.94@gmail.com>
2017-05-30 15:32:41 +0200

    Updated stacks and queues exercises - Problem 8 done

--- Stacks and Queues/StacksAndQueuesExercises/08.RecursiveFibonacci/RecursiveFibonacci.cs
+++ Stacks and Queues/StacksAndQueuesExercises/08.RecursiveFibonacci/RecursiveFibonacci.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace _08.RecursiveFibonacci
 {
-    class RecursiveFibonacci
+    public class RecursiveFibonacci
     {
-        static void Main(string[] args)
+        private static ulong[] lookUpArray;
+
+        public static void Main()
         {
+            lookUpArray = new ulong[50];
+            lookUpArray[0] = 1;
+            lookUpArray[1] = 1;
+
+            var n = ulong.Parse(Console.ReadLine());
+            var result = getFibonacci(n);
+
+            Console.WriteLine(result);
+        }
+
+        public static ulong getFibonacci(ulong n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+
+            if (lookUpArray[n - 1] != 0)
+            {
+                return lookUpArray[n - 1];
+            }
+
+            lookUpArray[n - 1] = getFibonacci(n - 1) + getFibonacci(n - 2);
+            return lookUpArray[n - 1];
         }
     }
 }
