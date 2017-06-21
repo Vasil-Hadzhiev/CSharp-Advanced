9f322402734fd0d7401aec9a4c49fd1688e34dc9
Vasil <vsl.h.94@gmail.com>
2017-06-21 22:41:38 +0200

    Completed Sets and Dictionaries exercises

--- Sets and Dictionaries/SetsAndDictionariesExercises/09.UserLogs/UserLogs.cs
+++ Sets and Dictionaries/SetsAndDictionariesExercises/09.UserLogs/UserLogs.cs
@@ -10,6 +10,41 @@ namespace _09.UserLogs
     {
         public static void Main()
         {
+            var logsByUser = new SortedDictionary<string, Dictionary<string, int>>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                var logInfo = input.Split(' ');
+                var IPinfo = logInfo[0].Split('=');
+                var IP = IPinfo[1];
+                var userInfo = logInfo[2].Split('=');
+                var user = userInfo[1];
+
+                if (!logsByUser.ContainsKey(user))
+                {
+                    logsByUser.Add(user, new Dictionary<string, int>());
+                    logsByUser[user].Add(IP, 0);
+                }
+                else if (!logsByUser[user].ContainsKey(IP))
+                {
+                    logsByUser[user].Add(IP, 0);
+                }
+                logsByUser[user][IP]++;
+            }
+            foreach (var outerPair in logsByUser)
+            {
+                Console.WriteLine("{0}:", outerPair.Key);
+                Console.WriteLine("{0}.", string.Join(", ", outerPair.
+                    Value.
+                    Select(kv => kv.Key + " => " + kv.Value)));
+            }
 
         }
     }
